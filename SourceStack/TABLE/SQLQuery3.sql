40d9da6f46b8eff412b01b1507d1736fd6f45a94
夏康平 <86136@DESKTOP-PC9C87Q>
2021-03-23 21:32:30 +0800

    完成USER表格的数据插入

---
+++ SourceStack/TABLE/SQLQuery3.sql
@@ -0,0 +1,20 @@
+﻿CREATE TABLE[DBO].[User](
+[id]int not null primary key,
+[username]nvarchar(50) null,
+[Password]nvarchar(50) null)
+drop table [user]
+insert [user](id,[username],[password])
+values (1,N'[17bang]',N'1234');
+
+ALTER TABLE [USER]
+ALTER COLUMN [PASSWORD]NVARCHAR(50)
+
+insert [user](id,[username],[password])
+VALUEs (2,N'Admin',null);
+
+insert [user](id,[username],[password])
+values (3,N'Admin-1','');
+
+insert [user](id,[username],[password])
+values (4,N'SuperAdmin',N'123456')
+
