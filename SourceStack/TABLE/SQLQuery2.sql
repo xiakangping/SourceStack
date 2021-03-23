246d5d849894bcb2d74077c0894eb7fb69ff88f9
夏康平 <86136@DESKTOP-PC9C87Q>
2021-03-23 20:33:28 +0800

    完成problem作业

---
+++ SourceStack/TABLE/SQLQuery2.sql
@@ -0,0 +1,12 @@
+﻿create table[dbo].Problem(
+[id] int not null primary key,
+[Content] text null,
+[NeedRemoteHelp] nvarchar(10)  null,
+Reward tinyint  null,
+[PublishDateTime] datetime  null,
+)
+
+drop table problem
+
+
+
