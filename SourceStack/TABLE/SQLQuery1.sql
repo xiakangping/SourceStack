031087d2d2c3264ccc5c3d8fbd4a39172b249ad2
夏康平 <86136@DESKTOP-PC9C87Q>
2021-03-30 10:07:03 +0800

    表结构作业

---
+++ SourceStack/TABLE/SQLQuery1.sql
@@ -0,0 +1,17 @@
+﻿--观察“一起帮”的注册和发布求助功能，试着建立表User：包含UserName（用户名），Password（密码）……
+create table [user](
+id int,
+UserName nvarchar(20),
+PassWord nvarchar(20)
+)
+--为User表添加一列：邀请人（InvitedBy），类型为INT
+alter table [user]
+ADD InvitedBy int
+
+--将InvitedBy类型修改为NVARCHAR(10)
+alter table [user]
+alter column InvitedBy nvarchar(10)
+
+--删除列InvitedBy
+alter table [user]
+drop  column InvitedBy 
