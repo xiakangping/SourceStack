4014ae9a85e4f8f735e6e79988f472b0f875e1ee
夏康平 <86136@DESKTOP-PC9C87Q>
2021-03-31 21:13:48 +0800

    完成约束

--- SourceStack/TABLE/SQLQuery1.sql
+++ SourceStack/TABLE/SQLQuery1.sql
@@ -1,36 +1,8 @@
-﻿
---在User表中：
+﻿--在User表上的基础上：
 
---查找没有录入密码的用户
---删除用户名（UserName）中包含“管理员”或者“17bang”字样的用户
-select * from [user] 
-where PassWord is null
+--添加Id列，让Id成为主键
+--添加约束，让UserName不能重复
+--在Problem表的基础上：
 
-delete from [user]
-where [UserName] =N'管理员';
-
---在Problem表中：
-
---给所有悬赏（Reward）大于10的求助标题加前缀：【推荐】
-update [Problem] set title=N'【推荐】'+title
-where Reward>10
---给所有悬赏大于20且发布时间（Created）在2019年10月10日之后的求助标题加前缀：【加急】
-update [Proble] set title=N'【加急】'+title
-where Reward>20 and NeedRemoteTime>'2019/10/10'
---删除所有标题以中括号【】开头（无论其中有什么内容）的求助
-delete [Problem] where title like N'【】'
---查找Title中第5个起，字符不为“数据库”且包含了百分号（%）的求助
-select * from [Problem]
-where title not like N'_____%数据库' and title like N'%#%%' escape '#'
-
---在Keyword表中：
-create table [keyword](
-[user] int,
-name nvarchar(20))
-
---找出所有被使用次数（Used）大于10小于50的关键字名称（Name）
-select [name] from keyword where used>10 and used<50 ;
---如果被使用次数（Used）小于等于0，或者是NULL值，或者大于100的，将其更新为1
-update keyword set used=1 where used<0 or used is null or used>100;
---删除所有使用次数为奇数的Keyword
-delete keyword where used=2%1;
+--为NeedRemoteHelp添加NOT NULL约束，再删除NeedRemoteHelp上NOT NULL的约束
+--添加自定义约束，让Reward不能小于10
--- SourceStack/TABLE/SQLQuery2.sql
+++ SourceStack/TABLE/SQLQuery2.sql
@@ -1,12 +1,29 @@
-﻿create table[dbo].Problem(
-[id] int not null primary key,
-[Content] text null,
-[NeedRemoteHelp] nvarchar(10)  null,
-Reward tinyint  null,
-[PublishDateTime] datetime  null,
-)
+﻿--在User表上的基础上：
+--添加Id列，让Id成为主键
+ALTER TABLE [user] DROP COLUMN id;
+ALTER TABLE [user]
+ADD id int ;
+ALTER TABLE [user] 
+ADD CONSTRAINT PK_Id PRIMARY KEY(Id); 
 
-drop table problem
+--添加约束，让UserName不能重复
+--在Problem表的基础上：
+ALTER TABLE [user]
+ADD CONSTRAINT UQ_UserName UNIQUE(UserName);
+
+--为NeedRemoteHelp添加NOT NULL约束，再删除NeedRemoteHelp上NOT NULL的约束
+--添加自定义约束，让Reward不能小于10
+alter table[Problem]
+alter column NeedRemoteHelp bit not null;
+alter table[Problem]
+alter column NeedRemoteHelp bit  null;
 
 
 
+alter table[Problem]
+drop NeedRemoteHelp;
+alter table[Problem]
+ADD CONSTRAINT CK_NeedRemoteHelp CHECK(NeedRemoteHelp>=10);
+
+select * from [user]
+select * from [Problem]
