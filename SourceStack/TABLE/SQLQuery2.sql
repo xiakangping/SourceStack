1041819816b567e309d1f71bdd0da087a80eb517
夏康平 <86136@DESKTOP-PC9C87Q>
2021-04-02 16:04:15 +0800

    完成select 作业

--- SourceStack/TABLE/SQLQuery2.sql
+++ SourceStack/TABLE/SQLQuery2.sql
@@ -1,27 +1,37 @@
-﻿--1.观察一起帮的“关键字”功能，新建Keyword表，要求带一个自增的主键Id，起始值为10，步长为5；并存入若干条数据
-alter table [keyword]
-add id int identity(10,5);
-INSERT INTO [keyword]([Name]) VALUES (N'陈国栋'); 
---2.将User表中Id列修改为可存储GUID的类型，并存入若干条包含GUID值的数据
-alter table [user]
-drop CONSTRAINT pk_id;
-alter table [user]
-drop column id;
-alter table [user]
-add id varchar(40);
-update [user]set id= (newid())
-
-
-select newid()
-
-
-
---3.Problem表已有Id列，如何给该列加上IDENTITY属性？
+﻿--1.在Problem中插入不同作者（Author）不同悬赏（Reward）的若干条数据，以便能完成以下操作：
 alter table [Problem]
-drop column id;
-alter table [Problem]
-add id int identity(1,1);
+add [Author] nvarchar(20); 
+update [Problem] set Author=N'飞哥'; 
+insert [Problem] (Peward,Author)
+values  (15,N'夏康平');
+
+--   1.查找出Author为“飞哥”的、Reward最多的3条求助
+select top 3 Peward from [Problem] where Author=N'飞哥'
+ORDER BY Peward DESC 
+
+--   2.所有求助，先按作者“分组”，然后在“分组”中按悬赏从大到小排序
+select author,peward from [Problem] order by
+author,peward desc
+--   3.查找并统计出每个作者的：求助数量、悬赏总金额和平均值
+select author,count(*),sum(peward),avg(peward) from [problem]
+group by author
+
+--   4.找出平均悬赏值少于10的作者并按平均值从小到大排序
+select author,avg(peward) from [problem] group by author
+having avg(peward)<10
+order by avg(peward)asc;
+--2.以Problem中的数据为基础，使用SELECT INTO，
+--新建一个Author和Reward都没有NULL值的新表：NewProblem 
+--（把原Problem里Author或Reward为NULL值的数据删掉）
+select author,peward into [NewProblem] from [Problem];
+
+--3.使用INSERT SELECT, 将Problem中Reward为NULL的行再次插入到NewProblem中
+update [NewProblem] set peward= null where author=N'大飞哥';
+insert [NewProblem](author,peward)
+select author,peward from [Problem] where peward is null；
+
+
+
 
 select * from [Problem];
-select * from [keyword];
-select * from [user];
+select * from [NewProblem];
