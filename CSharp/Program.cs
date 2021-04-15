f93a0f78b2a958f647558e404ba2d4531316283f
夏康平 <86136@DESKTOP-PC9C87Q>
2021-04-15 21:40:33 +0800

    完成循环取最大值最小值作业

--- CSharp/Program.cs
+++ CSharp/Program.cs
@@ -6,10 +6,23 @@ namespace CSharp
     {
         static void Main(string[] args)
         {
-            string[] names = { "夏康平", "李安平", "陈昂" };
-            for (int i = 0; i < names.Length; i++)
+            int[] studentsid = { 1, 2, 3, 4, 5, 7, 8, 9, 10, 15 };
+            
+            for (int i = 0; i < studentsid.Length; i++)
             {
-                Console.WriteLine(names[i]);
+                if (studentsid[i] == 5)
+                {
+                    Console.WriteLine($"找到了，在数组中第{(i + 1)}位");
+                    break;
+                }
+                else
+                {
+                    if (i==studentsid.Length-1)
+                    {
+                        Console.WriteLine("没找到");
+                    }
+                   
+                }/*else*/
             }
         }
     }
