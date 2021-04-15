d393b897b233fe4ad366ef3c478894a913c7cdb3
夏康平 <86136@DESKTOP-PC9C87Q>
2021-04-15 10:20:41 +0800

    完成for和while循环数组

--- CSharp/Program.cs
+++ CSharp/Program.cs
@@ -6,8 +6,11 @@ namespace CSharp
     {
         static void Main(string[] args)
         {
-            int score = 60;
-            Console.WriteLine(score);
+            string[] names = { "夏康平", "李安平", "陈昂" };
+            for (int i = 0; i < names.Length; i++)
+            {
+                Console.WriteLine(names[i]);
+            }
         }
     }
 }
