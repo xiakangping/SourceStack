3a4b3dfe9af06af25f9643d641381074be46077a
夏康平 <86136@DESKTOP-PC9C87Q>
2021-05-15 19:33:08 +0800

    完成枚举令牌管理

---
+++ HomeWord/TokenManage.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Homework
+{
+    class TokenManage
+    {
+        private Token _tokens;
+        public void Add(Token token)
+        {
+            _tokens = _tokens | token;
+        }
+        public void Remove(Token token)
+        {
+            if (Has(token))
+            {
+                _tokens = _tokens ^ token;
+            }
+            else
+            {
+                System.Console.WriteLine("Exception");
+            }
+        }
+        public bool Has(Token token)
+        {
+            return token == (_tokens & token);
+        }
+    }
+}
