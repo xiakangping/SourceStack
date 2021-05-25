0e1d0797735110247d83eb071b21ab59cd1d0e2c
夏康平 <86136@DESKTOP-PC9C87Q>
2021-05-25 20:23:24 +0800

    第二种回调方式

--- SourceStack/wwwroot/js/handler.js
+++ SourceStack/wwwroot/js/handler.js
@@ -1,15 +1,27 @@
-﻿//回调函数handler能对number进行运算，并返回bool值的，比如has6()
-function has6(number) {
-    var age = 6;
+﻿//////回调函数handler能对number进行运算，并返回bool值的，比如has6()
+////function has6(number) {
+////    var age = 6;
+////    if (number(age) == true) {
+////        console.log(true);
+////    } else {
+////        console.log(false);
+////    }
+////}
+
+////has6(handler);
+
+////function handler(age) {
+////    return age > 5;
+////}
+
+function handler(number) {
+    var age = 25;
     if (number(age) == true) {
         console.log(true);
     } else {
         console.log(false);
     }
 }
-
-has6(handler);
-
-function handler(age) {
-    return age > 5;
-}
+handler(function (age) {
+    return age > 60;
+});
