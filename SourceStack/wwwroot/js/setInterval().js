5ca9fc0b7db0a766987416862f049c11b900b770
夏康平 <86136@DESKTOP-PC9C87Q>
2021-05-30 20:04:15 +0800

    setTimeout()方法

---
+++ SourceStack/wwwroot/js/setInterval().js
@@ -0,0 +1,10 @@
+﻿var numb = 1;
+while (numb < 10) {
+    setTimeout(function () {
+        console.log("第" + numb + "周，源栈同学" + Math.floor(Math.random() * 100));
+        console.log(new Date());
+        numb += 1;
+    },
+        1000
+    );
+};
